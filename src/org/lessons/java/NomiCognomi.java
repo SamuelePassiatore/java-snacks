3791572d603bdaad2a8f5f3c93a793d6fa21ca62
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-05 10:58:52 +0200

    second snack init

---
+++ src/org/lessons/java/NomiCognomi.java
@@ -0,0 +1,12 @@
+package org.lessons.java;
+
+import java.util.Random;
+
+public class NomiCognomi {
+	public static void main(String[] args){
+		  String[] nomi = {"Bianca", "Lisa", "Barbara", "Lino", "Andrea", "Samuele"};
+		  String[] cognomi = {"Rossi", "Verdi", "Blu", "Marroni", "Bianchi", "Grigi"};
+		  
+		  Random random = new Random();
+	}
+}
