4d7a118e82e20721ea0e1c4bd5fb561fc7ce1db0
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-05 11:37:51 +0200

    third snack done

--- src/org/lessons/java/DispariInteri.java
+++ src/org/lessons/java/DispariInteri.java
@@ -2,7 +2,14 @@ package org.lessons.java;
 
 public class DispariInteri {
 	public static void main(String[] args){
-		  
-		 
+		int[] numeri = {1,2,3,4,5,6,7,8,9,10};
+		
+		int sum = 0;
+		
+		for (int i = 1; i < numeri.length; i += 2) {
+
+		      sum += numeri[i];
+		}
+		System.out.println("La somma degli elementi in posizione dispari è: " + sum);
 	}
 }
