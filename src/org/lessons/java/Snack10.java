fd786b616f54a62b22427176cbdbca3103190fb1
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-05 16:06:47 +0200

    tenth snack done

--- src/org/lessons/java/Snack10.java
+++ src/org/lessons/java/Snack10.java
@@ -7,10 +7,15 @@ public class Snack10 {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Inserisci un valore per il raggio: ");
-		int radius = sc.nextInt();
+		double radius = sc.nextDouble();
+		
+        double area = (radius * radius) * Math.PI;
+        double perimeter = (2 * radius) * Math.PI;
 		
 		sc.close();
 		
-		
+	    System.out.println("Il raggio del cerchio è: " + radius);
+        System.out.println("L'area del cerchio corrispondente è: " + area);
+        System.out.println("Il perimetro del cerchio corrispondente è: " + perimeter);
 	}
 }
