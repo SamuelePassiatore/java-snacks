b0e6743c78a56c1a2ab769ce129b68a676e99c53
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-05 10:48:45 +0200

    added scanner with ternary

--- src/org/lessons/java/PariDispari.java
+++ src/org/lessons/java/PariDispari.java
@@ -1,8 +1,20 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class PariDispari {
 	public static void main(String[] args){
-	  
-     
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dimmi il tuo numero:");
+		int val = sc.nextInt();
+		
+		sc.close();
+		
+		System.out.println(
+				val % 2 == 0
+				? val
+				: (val + 1)
+			);
 	}
 }
